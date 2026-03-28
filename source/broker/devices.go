99ad4326a6919b9e8989572c99a746d81a8c5458
WhiteSteps <simo.nole12@gmail.com>
2026-03-28 21:39:51 +0100

    moved broker directory into source

---
+++ source/broker/devices.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type Coordinates struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type Sensor struct {
+	ID           string      `json:"id"`
+	Name         string      `json:"name"`
+	Category     string      `json:"category"`
+	Region       string      `json:"region"`
+	Coordinates  Coordinates `json:"coordinates"`
+	WebSocketURL string      `json:"websocket_url"`
+}
+
+func getSensors() ([]Sensor, error) {
+	risposta, err := http.Get("http://localhost:8080/api/devices/")
+	if err != nil {
+		return nil, err
+	}
+	defer risposta.Body.Close()
+
+	body, err := io.ReadAll(risposta.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var sensors []Sensor
+	err = json.Unmarshal(body, &sensors)
+	if err != nil {
+		return nil, err
+	}
+
+	return sensors, nil
+}
